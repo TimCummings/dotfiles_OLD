8f459bd64fa532aae04dea7bc9441de6077d08b1
Tim Cummings <timjc86@gmail.com>
2021-01-24 10:55:19 -0600

    refactor: keybindings - minor adjustments
    * change enter to shift/enter (hold/tap)
    * add another 0 on Lower layer

--- keybindings/qmk/nyquist-r3/tjc/keymap.c
+++ keybindings/qmk/nyquist-r3/tjc/keymap.c
@@ -35,6 +35,7 @@ qk_tap_dance_action_t tap_dance_actions[] = {
 };
 
 #define GUI_ENT TD(TD_GUI_ENT)
+#define SFT_ENT SFT_T(KC_ENT)
 #define CTL_LFT LCTL(KC_LEFT)
 #define CTL_RGT LCTL(KC_RGHT)
 /* MacOS Specific */
@@ -63,7 +64,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS, \
   KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
-  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,  \
+  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, SFT_ENT, \
   KC_LCTL, KC_HYPR, KC_LALT, GUI_ENT, LOWER,   KC_SPC,  KC_BSPC, RAISE,   KC_RALT, KC_MEH,  KC_F3,   KC_RSFT  \
 ),
 
@@ -127,7 +128,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_GRV,  KC_SLSH, KC_7,    KC_8,    KC_9,    S(KC_8), _______, KC_LCBR, KC_LBRC, KC_RBRC, KC_RCBR, _______,  \
   _______, KC_MINS, KC_4,    KC_5,    KC_6,   S(KC_EQL),_______, _______, KC_LPRN, KC_RPRN,S(KC_MINS),_______, \
   _______, KC_DOT,  KC_1,    KC_2,    KC_3,    KC_EQL,  _______, _______, KC_LABK, KC_RABK, _______, _______,  \
-  _______, KC_COMM, KC_0,    _______, _______, _______, _______, _______, _______, _______, _______, _______   \
+  _______, KC_COMM, KC_0,    KC_0,    _______, _______, _______, _______, _______, _______, _______, _______   \
 ),
 
 /* Raise (Linux, maybe Windows too?)
